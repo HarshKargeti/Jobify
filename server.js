43b09f48f7022ee371cec7fe0ba224a8bccdc1a2
HarshitaKargeti <harshkargeti123@gmail.com>
2023-08-07 13:15:46 +0530

    update

---
+++ server.js
@@ -0,0 +1,47 @@
+import 'express-async-errors';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express'
+const app = express();
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+import jobRouter from './routes/jobRouter.js';
+
+import errorHandleMiddleware from './middleware/errorHandlerMiddleware.js';
+
+
+
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+
+app.use('/api/v1/jobs', jobRouter);
+
+app.use('*', (req, res) => {
+    res.status(404).json({ msg: 'not found' });
+});
+
+app.use(errorHandleMiddleware);
+
+const port = process.env.PORT || 5100
+
+
+try {
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(port, () => {
+        console.log("server")
+    });
+} catch (error) {
+    console.log(error);
+    process.exit(1);
+}
+
